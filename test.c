fbd9d8dbfe05fe410e53d569aa14b5c62f09883a
Alexandr Bezuglov <alexworldmail@gmail.com>
2016-06-28 14:37:24 +0000

    Stepic 5.4.10

--- test.c
+++ test.c
@@ -1,92 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define BUFFSIZE 1000
-#define RESULT_FILENAME "/result.out"
+#define IN_SOCKET "/in.fifo"
+#define OUT_SOCKET "/out.fifo"
 
-// inline std::string& ltrim(std::string& s, const char* t = " \t\n\r\f\v")
-// {
-//     s.erase(0, s.find_first_not_of(t));
-//     return s;
-// }
-
-// // trim from right
-// inline std::string& rtrim(std::string& s, const char* t = " \t\n\r\f\v")
-// {
-//     s.erase(s.find_last_not_of(t) + 1);
-//     return s;
-// }
-
-// // trim from left & right
-// inline std::string& trim(std::string& s, const char* t = " \t\n\r\f\v")
-// {
-//     return ltrim(rtrim(s, t), t);
-// }
-
-// void run_cmd (string cmdname, string params, int inp=0, int outp=0 ) {
-//     int pfd[2];
-//     pipe(pfd);
-//     if (inp) {
-//         close(pfd[0]);
-//         pfd[0] = inp;
-//     }
-//     if (outp) {
-//         close(pfd[1]);
-//         pfd[1] = outp;
-//     }
-//     if( !fork() ){
-//         close(STDIN_FILENO);
-//         dup2(pfd[0], STDIN_FILENO);
-//         close(STDOUT_FILENO);
-//         dup2(pfd[1], STDOUT_FILENO);
-//         close(pfd[0]);
-//         close(pfd[1]);
-//         execlp(cmdname, cmdname, params, NULL);
-//     } else {
-
-//     }
-    
-// }
 
 int main(int argc, char **argv) {
-    char localfilename[80];
-    sprintf(localfilename, "%s%s", getenv("HOME"), RESULT_FILENAME);
-    FILE *outfile = fopen(localfilename, "w+");
-    
-    char buff[BUFFSIZE], result[BUFFSIZE];
-    fgets(buff, BUFFSIZE, stdin);
-    
-    FILE *cmd = popen(buff, "r");
-    
-    while ( fgets(result, BUFFSIZE, cmd) != NULL ) {
-        fputs(result, outfile);
-    }
-    fclose (outfile);
-    //FILE *f = fopen(localfilename, "w");
-    // string input = "who | sort | uniq -c | sort -nk1";
-    // istringstream ss(input);
-    // string token;
-    
-    // cout << input << endl;
-    // while(std::getline(ss, token, '|')) {
-    //     token = trim(token);
-    //     cout << "--" << token << "--" << endl;
-    //     size_t found = token.find(" ");
-    //     string cmd_name, params;
-    //     if (found!=string::npos) {
-    //         cmd_name = token.substr(0, found);
-    //         params = token.substr(found);
-    //         trim(params);
-    //     }
-    //     else {
-    //         cmd_name = token;
-    //         params = "";
-    //     }
-    //     cout << "Cmdname --" << cmd_name << "--, params ++" << params << "++\n";
-        
-        
-    // }
+    char in_sock_name[80], out_sock_name[80];
+    sprintf(in_sock_name, "%s%s", getenv("HOME"), IN_SOCKET);
+    sprintf(out_sock_name, "%s%s", getenv("HOME"), OUT_SOCKET);
+    char buff[BUFFSIZE];
+    int res_in, res_out;
 
+    if ( (res_in=mkfifo(in_sock_name, 0444)) || (res_out=mkfifo(in_sock_name, 0666))) {
+        printf("%u  %u", res_in, res_out);
+//        return -1;
+    }
+
+        
+    FILE *in_sock = fopen(in_sock_name, "r");
+    FILE *out_sock = fopen(out_sock_name, "w");
     
+    while ( fgets(buff, BUFFSIZE, in_sock) != NULL)
+        fputs(buff, out_sock );
+    
+    fclose(in_sock);
+    fclose(out_sock);
     return 0;
 }
