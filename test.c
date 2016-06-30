825f8e0edf4df4c934c78d6f605fbebc5174086f
Alexandr Bezuglov <alexworldmail@gmail.com>
2016-06-30 11:59:28 +0000

    6.2.9

--- test.c
+++ test.c
@@ -11,6 +11,15 @@
 #define BUFFSIZE 1000
 #define OUT_FILE "/message.txt"
 
+#define CHECK(x) \
+    do { \
+        if (!(x)) { \
+            fprintf(stderr, "%s:%d: ", __func__, __LINE__); \
+            perror(#x); \
+            exit(-1); \
+        } \
+    } while (0)
+
 
 int main(int argc, char **argv) {
 
@@ -23,7 +32,7 @@ int main(int argc, char **argv) {
     mtr.mq_msgsize = BUFFSIZE;
     mtr.mq_curmsgs = 0;
     
-    int queue = mq_open("/test.mq", O_CREAT | O_RDWR, 0666, &mtr );
+    mqd_t queue = mq_open("/test.mq", O_CREAT | O_RDONLY, 0666, &mtr );
     printf ("Result queue = --%d--\n", queue);
     // key_t key = ftok("/tmp/msg.temp", 1);
     // int msgid = msgget (key, IPC_CREAT | 0666);
