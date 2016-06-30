4381447840df46e5ba5d3dcbf97316eb9fe17a2b
Alexandr Bezuglov <alexworldmail@gmail.com>
2016-06-30 19:41:37 +0000

    6.3.7

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>        /* For mode constants */
+#include <semaphore.h>
 
 #define BUFFSIZE 1000
 #define OUT_FILE "/message.txt"
@@ -11,27 +11,12 @@
 
 int main(int argc, char **argv) {
     
-    key_t key = ftok("/tmp/sem.temp", 1);
-    if( key == -1 ) {
-        perror( "ftok" );
-        return 1;
-    }
-    
-    int semid = semget(key, 16, IPC_CREAT | 0666); 
-    if( semid == -1 ) {
-        perror( "semget" );
-        return 1;
-    }
+    sem_unlink("/test.sem");
+    sem_t *sem = sem_open("/test.sem", O_CREAT | O_RDWR, 0666, 66);
+    // if( &sem == -1 ) {
+    //     perror( "sem_open" );
+    //     return 1;
+    // }
 
-    struct sembuf sops[16];
-    for (int i = 0; i < 16; i++) {
-        sops[i].sem_num = i;
-        sops[i].sem_op = i;
-        sops[i].sem_flg = 0;
-    }
-
-    semop(semid, sops, 16);
-
-    perror("Errno after semop: ");
     return 0;
 }
