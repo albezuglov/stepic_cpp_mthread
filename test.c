88a17417dd130970f34ac2567e37c6a15bab34b1
Alexandr Bezuglov <alexworldmail@gmail.com>
2016-06-30 11:40:11 +0000

    6.2.9

--- test.c
+++ test.c
@@ -4,29 +4,30 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <sys/stat.h>
+#include <fcntl.h>           /* For O_* constants */
+#include <mqueue.h>
 
-#define BUFFSIZE 80
+#define BUFFSIZE 1000
 #define OUT_FILE "/message.txt"
 
-struct message {
-    long type;
-    char value[BUFFSIZE];
-};
 
 int main(int argc, char **argv) {
 
-    struct message msg;
-    memset ( msg.value, 0, BUFFSIZE );
-
-    key_t key = ftok("/tmp/msg.temp", 1);
-    int msgid = msgget (key, IPC_CREAT | 0666);
+    char msg[BUFFSIZE];
+    memset ( msg, 0, BUFFSIZE );
     
-    msgrcv(msgid, &msg, BUFFSIZE, 0, 0 );
+    int queue = mq_open("/test.mq", O_RDONLY);
+
+    // key_t key = ftok("/tmp/msg.temp", 1);
+    // int msgid = msgget (key, IPC_CREAT | 0666);
+    
+    mq_receive(queue, msg, BUFFSIZE, 0 );
     {
         char out_filename[80];
         sprintf(out_filename, "%s%s", getenv("HOME"), OUT_FILE);
         FILE *out = fopen(out_filename, "w");
-        fputs(msg.value, out );
+        fputs(msg, out );
         fclose(out);
     }
 
